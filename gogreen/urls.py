d904590dd6a22e9e0ef68d409e665af830245e35
EC2 Default User <ec2-user@ip-172-31-8-64.eu-west-1.compute.internal>
2023-12-12 10:51:00 +0000

    SonarCloud check

--- gogreen/urls.py
+++ gogreen/urls.py
@@ -11,10 +11,7 @@ urlpatterns = [
     path('plant_list/', views.plant_list, name='plant_list'),
     path('purchase_plant/<int:plant_id>/', views.purchase_plant, name='purchase_plant'),
     path('update_plant/<int:plant_id>/', update_plant, name='update_plant'),
-    #path('order_table/', order_table, name='order_table'),
     path('delete_plant/<int:plant_id>/', delete_plant, name='delete_plant'),
-    #path('order_confirmation/<int:order_id>/', OrderConfirmationView.as_view(), name='order_confirmation'),
-    #path('delete_order/<int:order_id>/', delete_order, name='delete_order'),
     path('purchase/', purchase_plant, name='purchase_plant'),
     path('thank-you/', thank_you, name='thank_you'),
 ]
